59520e43d6b2c28e28b958532d53f428523b0522
Tony Jenkins <tony@tony-jenkins.co.uk>
2025-03-14 13:53:58 +0000

    Pricer V1

--- pizza_price.py
+++ pizza_price.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def order_price(basic_price, quantity, is_tuesday, needs_delivery):
-    pass
+
+    half_price_pizzas = quantity // 2 if is_tuesday else 0
+    full_price_pizzas = quantity - half_price_pizzas
+
+    total_price = (full_price_pizzas * basic_price) + (half_price_pizzas * (basic_price / 2))
+
+    if needs_delivery:
+        total_price += 2.5
+
+    return total_price
