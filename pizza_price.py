acc03fa3393aa2361fd26ce898401bf296e4821d
Tony Jenkins <tony@tony-jenkins.co.uk>
2025-03-14 13:38:37 +0000

    Initial setup

---
+++ pizza_price.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+def order_price(basic_price, quantity, is_tuesday, needs_delivery):
+    pass
---
+++ test_pizza.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import unittest
+
+from pizza_price import order_price
+
+class TestOrderPrice(unittest.TestCase):
+
+    def test_typical_order_not_tuesday_no_delivery(self):
+        self.assertEqual(order_price(10, 2, False, False), 20)
+
+
+if __name__ == '__main__':
+    unittest.main()
