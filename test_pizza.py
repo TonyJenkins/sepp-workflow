c8689189b4aca37f50fc5c628e4c1410ded2188b
Tony Jenkins <tony@tony-jenkins.co.uk>
2025-03-14 14:06:54 +0000

    Add Normal Day with Delivery Test

--- test_pizza.py
+++ test_pizza.py
@@ -9,6 +9,8 @@ class TestOrderPrice(unittest.TestCase):
     def test_typical_order_not_tuesday_no_delivery(self):
         self.assertEqual(order_price(10, 2, False, False), 20)
 
+    def test_typical_order_not_tuesday_with_delivery(self):
+        self.assertEqual(order_price(10, 2, False, False), 22.5)
 
 if __name__ == '__main__':
     unittest.main()
